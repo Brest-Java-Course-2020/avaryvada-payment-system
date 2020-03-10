41457670e707c7d94f6d971942fa0bc5df158d80
Aliaksei <alv96@bk.ru>
2020-03-10 08:24:52 +0300

    update: add operation model and dao interface

---
+++ payment-system-dao/src/main/java/com/epam/brest/courses/dao/OperationDao.java
@@ -0,0 +1,53 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.model.Operation;
+
+import com.epam.brest.courses.model.Operation;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OperationDao {
+
+    /**
+     * Find all Operations.
+     *
+     * @return Operations list.
+     */
+
+    List<Operation> findAll();
+
+    /**
+     * Find Operation by Id.
+     *
+     * @param  operationId -  Id.
+     * @return Operation
+     */
+
+    Optional<Operation> findById(Integer operationId);
+
+    /**
+     * Persist new  operation.
+     *
+     * @param operation .
+     * @return persisted operation id.
+     */
+    Integer create(Operation operation);
+
+    /**
+     * Update operation.
+     *
+     * @param operation .
+     * @return number of updated records in the database.
+     */
+    int update(Operation operation);
+
+    /**
+     * Delete operation.
+     *
+     * @param operationId .
+     * @return number of updated records in the database.
+     */
+    int delete(Integer operationId);
+
+}
--- payment-system-model/src/main/java/com/epam/brest/courses/model/Operation.java
+++ payment-system-model/src/main/java/com/epam/brest/courses/model/Operation.java
@@ -1,4 +1,52 @@
 package com.epam.brest.courses.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Operation {
+    private Integer operation_id;
+    private Integer customerCard_id;
+    private String description;
+    private Date operation_date;
+    private BigDecimal operation_cost;
+
+    public Integer getOperation_id() {
+        return operation_id;
+    }
+
+    public void setOperation_id(Integer operation_id) {
+        this.operation_id = operation_id;
+    }
+
+    public Integer getCustomerCard_id() {
+        return customerCard_id;
+    }
+
+    public void setCustomerCard_id(Integer customerCard_id) {
+        this.customerCard_id = customerCard_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getOperation_date() {
+        return operation_date;
+    }
+
+    public void setOperation_date(Date operation_date) {
+        this.operation_date = operation_date;
+    }
+
+    public BigDecimal getOperation_cost() {
+        return operation_cost;
+    }
+
+    public void setOperation_cost(BigDecimal operation_cost) {
+        this.operation_cost = operation_cost;
+    }
 }
